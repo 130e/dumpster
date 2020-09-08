6491b6b9ffb0ec15ffa4b607f908f44f31a58669
130e <fernival328@gmail.com>
2020-09-08 16:57:21 -0700

    clear iptables

---
+++ clear_iptables.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# must be root
+iptables-save | awk '/^[*]/ { print $1 } 
+                     /^:[A-Z]+ [^-]/ { print $1 " ACCEPT" ; }
+                     /COMMIT/ { print $0; }' | iptables-restore
